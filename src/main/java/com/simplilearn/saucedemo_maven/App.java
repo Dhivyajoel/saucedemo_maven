8b55028e0c76e7fa9887f96d4566b2cece9aae82
Dhivyajoel <chat2joe86@gmail.com>
2022-09-22 13:37:48 +0000

    saucedemo_maven

---
+++ src/main/java/com/simplilearn/saucedemo_maven/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.saucedemo_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
